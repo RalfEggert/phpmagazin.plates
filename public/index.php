7369a84033f30bcb58027e70ad84feeaec2da05d
Ralf Eggert <ralf@travello.com>
2017-02-01 21:18:01 +0100

    Initial Commit

---
+++ public/index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * PHP Magazin: Artikel über Plates
+ *
+ * @author     Ralf Eggert <r.eggert@travello.de>
+ * @link       https://www.travello.de/
+ * @copyright Travello GmbH, 2017
+ */
+
+phpinfo();
