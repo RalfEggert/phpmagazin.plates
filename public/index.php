18e5bf33afc9ac75cf45099b496d552a4ebadc52
Ralf Eggert <ralf@travello.com>
2017-02-01 21:41:28 +0100

    refactored first example

--- public/index.php
+++ public/index.php
@@ -4,7 +4,19 @@
  *
  * @author     Ralf Eggert <r.eggert@travello.de>
  * @link       https://www.travello.de/
- * @copyright Travello GmbH, 2017
+ * @copyright  Travello GmbH, 2017
  */
 
-phpinfo();
+use League\Plates\Engine;
+
+define('PROJECT_ROOT', realpath(__DIR__ . '/..'));
+
+require_once PROJECT_ROOT . '/vendor/autoload.php';
+
+$engine = new Engine(null, 'phtml');
+$engine->addFolder('layouts', PROJECT_ROOT . '/templates/layouts');
+$engine->addFolder('pages', PROJECT_ROOT . '/templates/pages');
+
+$template = $engine->make('pages::page1');
+
+echo $template->render(['name' => 'Ralf']);
